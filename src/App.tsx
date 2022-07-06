339735bcef6f1e3de693c2a131ec04ba76bf0e94
Guilherme Vieira de Paiva <guivpw68@gmail.com>
2022-07-06 01:27:46 -0300

    feat: add root component

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import { StatusBar } from 'expo-status-bar'
+import { SafeAreaProvider } from 'react-native-safe-area-context'
+import { registerRootComponent } from 'expo'
+
+import useCachedResources from './hooks/useCachedResources'
+import Navigation from './navigation'
+
+export default function App() {
+  const isLoadingComplete = useCachedResources()
+
+  if (!isLoadingComplete) {
+    return null
+  } else {
+    return (
+      <SafeAreaProvider>
+        <Navigation />
+        <StatusBar />
+      </SafeAreaProvider>
+    )
+  }
+}
+
+registerRootComponent(App)
